b3064c492ab5c2a2da32ab9bdfcf3a5436f6b6a5
Minjun Kim (Lapis0875) <lapis0875@kakao.com>
2023-01-02 11:01:59 +0000

    feat: Change config

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -1,35 +1,43 @@
 const CONFIG = {
   // profile setting
   profile: {
-    name: 'morethanmin',
+    name: 'lapis',
     image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: 'frontend developer',
-    bio: 'I develop everything using node.',
-    email: 'morethanmin.dev@gmail.com',
-    github: 'morethanmin',
+    role: '학생 개발자',
+    bio: '대학생 개발자.',
+    email: 'lapis0875@gmail.com',
+    github: 'lapis0875',
     linkedin: '',
     instagram: '',
   },
   projects: [
     {
-      name: 'Untilled',
-      href: 'https://untilled.web.app'
+      name: 'Python Tutorials',
+      href: 'https://github.com/KoreanTutorials/PythonTutorial'
+    },
+    {
+      name: 'SimpleHome (Minecraft JE Paper Plugin)',
+      href: 'https://github.com/Lapis0875/SimpleHome'
+    },
+    {
+      name: 'SimpleLock (Minecraft JE Paper Plugin)',
+      href: 'https://github.com/Lapis0875/SimpleLock'
     }
   ],
   // blog setting
   blog: {
-    title: 'morethan-log',
-    description: 'welcome to morethan-log!',
+    title: 'lazuli',
+    description: 'welcome to lazuli!',
     theme: 'auto' // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration
-  link: 'https://morethan-log.vercel.app',
-  since: 2022, // If leave this empty, current year will be used.
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: 'https://lazuli-blog.vercel.app',
+  since: 2023, // If leave this empty, current year will be used.
+  lang: 'ko-KR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', 'Website', 'Notion', 'Life', 'Dev', 'Programming', 'Food', 'Desert', 'Pictures'],
   },
 
   // notion configuration
@@ -53,7 +61,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: 'morethanmin/morethan-log',
+      repo: 'Lapis0875/lazuli-blog',
       'issue-term': 'og:title',
       label: '💬 Utterances',
     }
